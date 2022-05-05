7968f8fdc532fd1ffc41f58c46d360ee686bf4c7
Abraham Egnor <abraham.egnor@mongodb.com>
2022-05-05 16:35:56 -0400

    record steps for bindgen

---
+++ bindings/rust/bindgen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ../..
+mkdir cmake-build
+cmake . -Bcmake-build
+bindgen cmake-build/src/mongocrypt.h \
+    -o bindings/rust/src/bindings.rs \
+    --allowlist-function mongocrypt_version \
+    -- -I src
