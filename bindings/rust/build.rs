9e38347c274978afbad319394ff9f7b1c6c72eae
Abraham Egnor <abraham.egnor@mongodb.com>
2022-05-26 14:25:07 -0400

    static support libraries

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -3,23 +3,22 @@ use std::env;
 fn main() {
     const LIB_DIR: &str = "MONGOCRYPT_LIB_DIR";
     const STATIC: &str = "MONGOCRYPT_STATIC";
-    const NAME: &str = "MONGOCRYPT_LIB_NAME";
-    for var in &[LIB_DIR, STATIC, NAME] {
+    for var in &[LIB_DIR, STATIC] {
         println!("cargo:rerun-if-env-changed={}", var);
     }
 
-    let name = if let Ok(s) = env::var(NAME) {
-        s
+    let is_static = env::var(STATIC).is_ok();
+    let (name, kind) = if is_static {
+        ("mongocrypt-static", "static")
     } else {
-        "mongocrypt".to_string()
-    };
-    let kind = if let Ok(_) = env::var(STATIC) {
-        "static"
-    } else {
-        "dylib"
+        ("mongocrypt", "dylib")
     };
 
     println!("cargo:rustc-link-lib={}={}", kind, name);
+    if is_static {
+        println!("cargo:rustc-link-lib=static=bson-static");
+        println!("cargo:rustc-link-lib=static=kms_message-static");
+    }
     if let Ok(path) = env::var(LIB_DIR) {
         println!("cargo:rustc-link-search=native={}", path);
     }
