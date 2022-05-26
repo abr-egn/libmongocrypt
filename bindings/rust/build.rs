955295f7fd97c0bcf142103e6e8455b43e19adcc
Abraham Egnor <abraham.egnor@mongodb.com>
2022-05-26 14:16:42 -0400

    wip

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -3,16 +3,23 @@ use std::env;
 fn main() {
     const LIB_DIR: &str = "MONGOCRYPT_LIB_DIR";
     const STATIC: &str = "MONGOCRYPT_STATIC";
-    for name in &[LIB_DIR, STATIC] {
-        println!("cargo:rerun-if-env-changed={}", name);
+    const NAME: &str = "MONGOCRYPT_LIB_NAME";
+    for var in &[LIB_DIR, STATIC, NAME] {
+        println!("cargo:rerun-if-env-changed={}", var);
     }
 
-    let mut kind = "dylib";
-    if let Ok(_) = env::var(STATIC) {
-        kind = "static";
-    }
-    println!("cargo:rustc-link-lib={}=mongocrypt", kind);
+    let name = if let Ok(s) = env::var(NAME) {
+        s
+    } else {
+        "mongocrypt".to_string()
+    };
+    let kind = if let Ok(_) = env::var(STATIC) {
+        "static"
+    } else {
+        "dylib"
+    };
 
+    println!("cargo:rustc-link-lib={}={}", kind, name);
     if let Ok(path) = env::var(LIB_DIR) {
         println!("cargo:rustc-link-search=native={}", path);
     }
