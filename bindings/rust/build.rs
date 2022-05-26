6faa482d6bd002a6251d1942ed5f33c83b2f34f4
Abraham Egnor <abraham.egnor@mongodb.com>
2022-05-26 14:08:41 -0400

    more configurable native linking

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,3 +1,19 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-lib=mongocrypt");
+    const LIB_DIR: &str = "MONGOCRYPT_LIB_DIR";
+    const STATIC: &str = "MONGOCRYPT_STATIC";
+    for name in &[LIB_DIR, STATIC] {
+        println!("cargo:rerun-if-env-changed={}", name);
+    }
+
+    let mut kind = "dylib";
+    if let Ok(_) = env::var(STATIC) {
+        kind = "static";
+    }
+    println!("cargo:rustc-link-lib={}=mongocrypt", kind);
+
+    if let Ok(path) = env::var(LIB_DIR) {
+        println!("cargo:rustc-link-search=native={}", path);
+    }
 }
