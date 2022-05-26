a4d0754b322dd32c04e4908ee4b7f08cb97934be
Abraham Egnor <abraham.egnor@mongodb.com>
2022-05-26 14:31:50 -0400

    drop static

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,23 +2,9 @@ use std::env;
 
 fn main() {
     const LIB_DIR: &str = "MONGOCRYPT_LIB_DIR";
-    const STATIC: &str = "MONGOCRYPT_STATIC";
-    for var in &[LIB_DIR, STATIC] {
-        println!("cargo:rerun-if-env-changed={}", var);
-    }
+    println!("cargo:rerun-if-env-changed={}", LIB_DIR);
 
-    let is_static = env::var(STATIC).is_ok();
-    let (name, kind) = if is_static {
-        ("mongocrypt-static", "static")
-    } else {
-        ("mongocrypt", "dylib")
-    };
-
-    println!("cargo:rustc-link-lib={}={}", kind, name);
-    if is_static {
-        println!("cargo:rustc-link-lib=static=bson-static");
-        println!("cargo:rustc-link-lib=static=kms_message-static");
-    }
+    println!("cargo:rustc-link-lib=dylib=mongocrypt");
     if let Ok(path) = env::var(LIB_DIR) {
         println!("cargo:rustc-link-search=native={}", path);
     }
